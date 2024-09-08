44361b0dbb19781a5e166cb9e56daa4b1b37b8d1
KodaiYamashita <nglcobdai@gmail.com>
2024-09-08 18:38:09 +0900

    feat: pytest

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -1,14 +1,14 @@
-from PIL import Image
 import numpy as np
+from text_to_image.utils import settings
+from PIL import Image
+
 from text_to_image.api.model import TextToImageModel
-from text_to_image.utils import logger
 
 
 class TestGenerate:
     def setup_class(self):
-        self.generator = TextToImageModel(use_gpu=True)
+        self.generator = TextToImageModel(use_gpu=settings.USE_GPU)
         self.image = self.generator("A beautiful sunset", is_save=False)
-        logger.info("Generate image")
 
     def test_generate_image(self):
         assert isinstance(self.image, Image.Image)
