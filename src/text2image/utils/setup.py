0d3f3f2615b91fd420014d2bcbfc29534be70d5b
KodaiYamashita <nglcobdai@gmail.com>
2024-12-29 22:08:22 +0900

    fix: config/message.ini

--- src/text2image/utils/setup.py
+++ src/text2image/utils/setup.py
@@ -8,6 +8,8 @@ from nglcobdai_utils import (
 
 settings = Settings()
 ch = ConsoleHandlerInfo(log_level="INFO")
-fh = RotatingFileHandlerInfo(log_level="DEBUG", filename="/datadrive/logs/text2image.log")
+fh = RotatingFileHandlerInfo(
+    log_level="DEBUG", filename="/datadrive/logs/text2image.log"
+)
 logger = get_logger(name=settings.PROJECT_NAME, ch_info=ch, fh_info=fh)
-messenger = Messenger("src/config/message.ini")
+messenger = Messenger("config/message.ini")
